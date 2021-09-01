34c1830eb17966cc3b086be92315d5f175f3ade5
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2021-09-01 02:18:32 -0400

    Add WLED logo in top bar

--- wlednativeios/Views/ContentView.swift
+++ wlednativeios/Views/ContentView.swift
@@ -2,10 +2,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    var deviceList = DeviceRepository.instance.getAll()
+    
     var body: some View {
-        List(DeviceRepository.instance.getAll(), id: \.address) { deviceItem in
-            DeviceListItem(device: deviceItem)
+        NavigationView {
+            List(deviceList, id: \.address) {
+                deviceItem in DeviceListItem(device: deviceItem)
+            }
             
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar(content: {
+                ToolbarItem(placement: .principal) {
+                    Image("wled_logo_akemi")
+                }
+            })
         }
     }
 }
@@ -13,7 +23,11 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            ContentView()
+            ContentView(deviceList: [
+                DeviceItem(address: "192.168.10.194", name: "WLED Bedroom"),
+                DeviceItem(address: "192.168.10.195", name: "WLED Kitchen"),
+                DeviceItem(address: "192.168.10.196", name: "WLED Kitchen 2")
+            ])
         }
     }
 }
--- wlednativeios/Views/DeviceListItem.swift
+++ wlednativeios/Views/DeviceListItem.swift
@@ -4,15 +4,21 @@ import SwiftUI
 struct DeviceListItem: View {
     var device: DeviceItem
     
+    @State private var brightness: Double = 50
+    
     var body: some View {
         HStack {
             VStack(alignment: .leading) {
-                Text(device.name)
+                Text(device.name != "" ? device.name : "(New Device)")
                     .font(.title2)
                     .lineLimit(1)
                 Text(device.address)
                     .font(.body)
                     .lineLimit(1)
+                Slider(
+                    value: $brightness,
+                    in: 0...255
+                )
                     
             }
             Spacer()
@@ -26,6 +32,6 @@ struct DeviceListItem_Previews: PreviewProvider {
             DeviceListItem(device: DeviceItem(address: "192.168.10.194", name: "WLED Kitchen"))
             DeviceListItem(device: DeviceItem(address: "4.3.2.1", name: "WLED AP"))
         }
-        .previewLayout(.fixed(width: 300, height: 70))
+        .previewLayout(.fixed(width: 300, height: 110))
     }
 }
