dce4b14fa5d6e72e7ed1acad4ead341bd19f72d9
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2023-02-19 04:12:56 -0500

    Improve new device screen
    Add extra validation

--- wled-native/EntryViewController.swift
+++ wled-native/EntryViewController.swift
@@ -4,6 +4,7 @@ class EntryViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var addressField: UITextField!
     @IBOutlet var nameField: UITextField!
+    @IBOutlet var isHiddenLabel: UILabel!
     @IBOutlet var isHiddenSwitch: UISwitch!
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
@@ -11,24 +12,85 @@ class EntryViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTextFields()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveDevice))
+        navigationItem.rightBarButtonItem?.isEnabled = false
+    }
+    
+    func setupTextFields() {
         addressField.delegate = self
         nameField.delegate = self
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveDevice))
+        addressField.addTarget(self,
+                               action: #selector(self.textFieldDidChange(_:)),
+                               for: UIControl.Event.editingChanged)
+        nameField.addTarget(self,
+                            action: #selector(self.textFieldDidChange(_:)),
+                            for: UIControl.Event.editingChanged)
+        
+        addressField.layer.borderColor = UIColor.systemRed.cgColor
+        nameField.layer.borderColor = addressField.layer.borderColor
+        
+        let webImage = UIImage(systemName: "link")
+        addressField.leftView = getPaddedImageView(webImage!)
+        addressField.leftViewMode = UITextField.ViewMode.always
+        
+        let nameImage = UIImage(systemName: "heart.text.square")
+        nameField.leftView =  getPaddedImageView(nameImage!)
+        nameField.leftViewMode = UITextField.ViewMode.always
+        
+        let attachment = NSTextAttachment()
+        attachment.image = UIImage(systemName: "eye.slash")
+        let attachmentString = NSMutableAttributedString(attachment: attachment)
+        let myString = NSMutableAttributedString(string: " " + isHiddenLabel.text!)
+        attachmentString.append(myString)
+        isHiddenLabel.attributedText = attachmentString
+    }
+    
+    func getPaddedImageView(_ imageView: UIImage) -> UIView {
+        let padding = 8.0
+        let imageView = UIImageView(image: imageView)
+        imageView.tintColor = UIColor.placeholderText
+        let outerView = UIView(frame: CGRect(x: 0, y: 0, width: imageView.image!.size.width + padding, height: imageView.image!.size.height) )
+        imageView.frame = CGRectMake(padding, 0.0, imageView.image!.size.width, imageView.image!.size.height)
+        outerView.addSubview(imageView)
+        return outerView
     }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        saveDevice()
+        if (navigationItem.rightBarButtonItem!.isEnabled) {
+            saveDevice()
+        }
         return true
     }
     
-    @objc func saveDevice() {
+    func isAddressValid() -> Bool {
         // TODO Add validation that the address doesnt return nil when passed to URL(string:)
+        return !(addressField?.text?.isEmpty ?? false)
+    }
+    
+    func isNameValid() -> Bool {
+        return true
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
         // TODO Add error message in the interface
-        guard let address = addressField.text, !address.isEmpty else {
+        let addressIsValid = isAddressValid()
+        let nameIsValid = isNameValid()
+        navigationItem.rightBarButtonItem?.isEnabled = addressIsValid && nameIsValid
+        
+        addressField.layer.borderWidth = addressIsValid ? 0 : 1
+        nameField.layer.borderWidth = nameIsValid ? 0 : 1
+    }
+    
+    @objc func saveDevice() {
+        guard let address = addressField.text, isAddressValid() else {
             return
         }
-        let name = nameField.text
+        guard let name = nameField.text, isNameValid() else {
+            return
+        }
         
         let device = Device(context: context)
         device.address = address
