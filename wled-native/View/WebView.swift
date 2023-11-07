55e5227e31924f61f68e0d3c7a9be3f3e71fbda5
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2023-11-07 02:21:16 -0500

    Add custom error page

--- wled-native/View/WebView.swift
+++ wled-native/View/WebView.swift
@@ -4,10 +4,15 @@ import WebKit
 
 struct WebView: UIViewRepresentable {
     
+    var webView: WKWebView = WKWebView()
     var url: URL?
     
+    init(url: URL? = nil) {
+        self.url = url
+    }
+    
     func makeUIView(context: Context) -> WKWebView {
-        return WKWebView()
+        return webView
     }
     
     func updateUIView(_ webView: WKWebView, context: Context) {
@@ -15,6 +20,26 @@ struct WebView: UIViewRepresentable {
             return
         }
         let request = URLRequest(url: url)
+        webView.uiDelegate = context.coordinator
+        webView.navigationDelegate = context.coordinator
         webView.load(request)
+    }
+    
+    class Coordinator: NSObject, WKUIDelegate, WKNavigationDelegate {
+        var parent: WebView
+
+        init(_ parent: WebView) {
+            self.parent = parent
+        }
+        
+        func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+            let htmlPath = Bundle.main.path(forResource: "errorPage", ofType: "html")
+            let htmlUrl = URL(fileURLWithPath: htmlPath!, isDirectory: false)
+            webView.loadFileURL(htmlUrl, allowingReadAccessTo: htmlUrl)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
     }
 }
