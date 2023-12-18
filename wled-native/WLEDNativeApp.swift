b1cc1621861e8173c2f760366f0b70fb485a1d0b
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2023-12-18 02:04:40 -0500

    Check for new releases once per 24 hours

--- wled-native/WLEDNativeApp.swift
+++ wled-native/WLEDNativeApp.swift
@@ -18,8 +18,23 @@ struct WLEDNativeApp: App {
     
     private func refreshVersionsSync() {
         Task {
+            // Only update automatically from Github once per 24 hours to avoid rate limits
+            // and reduce network usage.
+            let dateLastUpdateKey = "lastUpdateReleasesDate"
+            let date = Date(timeIntervalSince1970: UserDefaults.standard.double(forKey: dateLastUpdateKey))
+            var dateComponent = DateComponents()
+            dateComponent.day = 1
+            let dateToRefresh = Calendar.current.date(byAdding: dateComponent, to: date)
+            let dateNow = Date()
+            guard let dateToRefresh = dateToRefresh else {
+                return
+            }
+            if (dateNow <= dateToRefresh) {
+                return
+            }
             print("Refreshing available Releases")
             await ReleaseService().refreshVersions(context: persistenceController.container.viewContext)
+            UserDefaults.standard.set(Date().timeIntervalSince1970, forKey: dateLastUpdateKey)
         }
     }
 }
